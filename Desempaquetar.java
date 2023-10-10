3f32e9729c8f575eced164ae9747ed5ac8c63caf
ato1905 <xxzelda10xx@gmail.com>
2023-10-10 12:57:40 +0200

    10-10

---
+++ Desempaquetar.java
@@ -0,0 +1,160 @@
+import java.io.*;
+
+import java.security.*;
+import java.security.interfaces.RSAKey;
+import java.security.spec.*;
+
+import javax.crypto.*;
+import javax.crypto.interfaces.*;
+import javax.crypto.spec.*;
+
+import org.bouncycastle.asn1.pkcs.RSAESOAEPparams;
+import org.bouncycastle.jcajce.provider.asymmetric.RSA;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class Desempaquetar {
+    public static void main(String[] args) throws Exception {
+    // TODO code application logic here
+    desempaquetar("paquete.bin");
+    }
+
+
+
+    public static void desempaquetar(String nombreFichero) throws Exception {
+        Paquete paquete = new Paquete();
+        paquete.leerPaquete(nombreFichero);
+
+        byte[] examenCifrado = paquete.getContenidoBloque("examenCifrado");
+        byte[] claveCifrada = paquete.getContenidoBloque("claveCifrada");
+        byte[] hashCifrado = paquete.getContenidoBloque("hashCifrado");
+
+
+        //Descifrar clave
+        byte[] claveDescifrada = descifrarClave(claveCifrada);
+        System.out.println("Clave descifrada: " + claveDescifrada);
+        System.out.println(bytesToHex(claveDescifrada));
+
+        //Descifrar examen
+        byte[] examenDescifrado = descifrarExamen(claveDescifrada, examenCifrado);
+        System.out.println("Examen descifrado: " + examenDescifrado);
+        System.out.println(bytesToHex(examenDescifrado));
+
+        //Hash de la claveSecreta cifrada
+        byte[] hashClaveSecretaExamenCifrado = hashClaveSecretaCifrada(claveCifrada, examenCifrado);
+        System.out.println("Hash: " + hashClaveSecretaExamenCifrado);
+        System.out.println(bytesToHex(hashClaveSecretaExamenCifrado));
+
+        //Descifrar hash
+        byte[] hashDescifrado = descifrarHashClavePrivadaAlumno(hashCifrado);
+        System.out.println("Hash descifrado: " + hashDescifrado);
+        System.out.println(bytesToHex(hashDescifrado));
+
+        //Verificar hash
+        if(verificarHash(hashDescifrado,hashClaveSecretaExamenCifrado)){
+            System.out.println("El hash es correcto");
+        } else {
+            System.out.println("El hash no es correcto");
+        };
+        
+    }
+    
+
+
+    public static byte[] descifrarClave(byte[] claveCifrada) throws Exception{
+        // Cargar clave privada del profesor desde el archivo "profesor.privada"
+        FileInputStream privateKeyFile = new FileInputStream("profesor.privada");
+        byte[] privateKeyBytes = new byte[privateKeyFile.available()];
+        privateKeyFile.read(privateKeyBytes);
+        privateKeyFile.close();
+
+        KeyFactory keyFactoryRSA = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        PrivateKey clavePrivadaProfesor = keyFactoryRSA.generatePrivate(privateKeySpec);
+
+        // Descifrar clave
+        Cipher cifrador = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cifrador.init(Cipher.DECRYPT_MODE, clavePrivadaProfesor);
+        byte[] datosDescifrados = cifrador.doFinal(claveCifrada);
+        
+        return datosDescifrados;
+    }
+    
+    public static byte[] descifrarExamen(byte[] claveDescifrada, byte[] examenCifrado) throws Exception{
+        //Generar clave secreta
+        SecretKey claveDescifradaKey = new SecretKeySpec(claveDescifrada, 0, claveDescifrada.length, "DES");
+        
+        //Descifrar examen
+        Cipher cifrador = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        cifrador.init(Cipher.DECRYPT_MODE, claveDescifradaKey);
+        byte[] examenDescifrado = cifrador.doFinal(examenCifrado);
+        
+        return examenDescifrado;
+    }
+
+    public static byte[] descifrarHashClavePrivadaAlumno(byte[] hashCifrado ) throws Exception{
+        // Cargar clave privada del alumno desde el archivo "alumno.privada"
+        FileInputStream publicKeyFile = new FileInputStream("alumno.publica");
+        byte[] publicKeyBytes = new byte[publicKeyFile.available()];
+        publicKeyFile.read(publicKeyBytes);
+        publicKeyFile.close();
+
+        KeyFactory keyFactoryRSA = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        PublicKey clavePublicaAlumno = keyFactoryRSA.generatePublic(publicKeySpec);
+
+        // Descifrar hash
+        Cipher cifrador = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cifrador.init(Cipher.DECRYPT_MODE, clavePublicaAlumno);
+        byte[] datosDescifrados = cifrador.doFinal(hashCifrado);
+        
+        return datosDescifrados;
+        //a7bf3bb4490bfbf0ab46a8b517ba8059cf867e3f87c0a747f0e697b3e4b4e0fd
+    }
+
+
+    public static byte[] hashClaveSecretaCifrada(byte[] claveCifrada, byte[] examenCifrado) throws Exception{ //parametro a pasar???
+        //Calcular hash del examen
+        MessageDigest hash = MessageDigest.getInstance("SHA-256");
+        hash.update(examenCifrado);
+        hash.update(claveCifrada);
+        byte[] hashClaveCifradaExamenCirfrado = hash.digest();
+        
+
+        return hashClaveCifradaExamenCirfrado;
+    }
+
+    public static boolean verificarHash(byte[] hashDescifrado, byte[] hashClaveSecretaExamenCifrado) throws Exception{
+        FileInputStream publicKeyFile = new FileInputStream("alumno.publica");
+        byte[] publicKeyBytes = new byte[publicKeyFile.available()];
+        publicKeyFile.read(publicKeyBytes);
+        publicKeyFile.close();
+
+        KeyFactory keyFactoryRSA = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        PublicKey clavePublicaAlumno = keyFactoryRSA.generatePublic(publicKeySpec);
+
+        Signature verificado = Signature.getInstance("SHA256withRSA");
+        verificado.initVerify(clavePublicaAlumno);
+        verificado.update(hashClaveSecretaExamenCifrado);
+        if(verificado.verify(hashDescifrado, 0, 32)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+    
+    
+}
+
+
--- Empaquetar.java
+++ Empaquetar.java
@@ -1,78 +1,53 @@
 import java.io.*;
 
 import java.security.*;
+import java.security.interfaces.RSAKey;
 import java.security.spec.*;
 
 import javax.crypto.*;
 import javax.crypto.interfaces.*;
 import javax.crypto.spec.*;
 
+import org.bouncycastle.asn1.pkcs.RSAESOAEPparams;
+import org.bouncycastle.jcajce.provider.asymmetric.RSA;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Empaquetar{
     public static void main(String[] args) throws Exception {
-        Security.addProvider(new BouncyCastleProvider());
-        KeyFactory keyFactoryRSA = KeyFactory.getInstance("RSA", "BC");
-        
-        //Cargar clave privada del alumno
-        File ficheroClavePrivada = new File("alumno.privada"); 
-		int tamanoFicheroClavePrivada = (int) ficheroClavePrivada.length();
-		byte[] bufferPriv = new byte[tamanoFicheroClavePrivada];
-		FileInputStream in = new FileInputStream(ficheroClavePrivada);
-		in.read(bufferPriv, 0, tamanoFicheroClavePrivada);
-		in.close();
+        //Generar secretKey
+        SecretKey claveSecreta = generaSecretKey();
 
-		// 2.2 Recuperar clave privada desde datos codificados en formato PKCS8
-		PKCS8EncodedKeySpec clavePrivadaSpec = new PKCS8EncodedKeySpec(bufferPriv);
-		PrivateKey clavePrivada2 = keyFactoryRSA.generatePrivate(clavePrivadaSpec);
-    
-        //Crear cifrador que empaquete el examen con clave privada del profesor
-        Cipher cifrador = Cipher.getInstance("DES", "BC");
-        cifrador.init(Cipher.ENCRYPT_MODE, clavePrivada2);
-    
-        //Cargar examen.pdf
-        in = new FileInputStream("examen.pdf");
-        byte[] examen = new byte[in.available()];
-        in.read(examen);
-        in.close();
-    
         //Cifrar examen
-        cifrador.update(examen);
-        
-        //Escribir examen cifrado en archivo
-        FileOutputStream out = new FileOutputStream("examen.cifrado");
-        out.write(cifrador.doFinal());
-        out.close();    
-        
-       
+        byte[] examenCifrado = cifrarExamen(claveSecreta, "examen.txt");
+        System.out.println("Examen cifrado: " + examenCifrado);
+        System.out.println(bytesToHex(examenCifrado));
 
-/*
-        //Cargar examen.pdf
-        in = new FileInputStream("examen.pdf");
-        byte[] examen = new byte[in.available()];
-        in.read(examen);
-        in.close();
-        
-        //Firmar examen
-        firma.update(examen);
-        byte[] firmaExamen = firma.sign();
+        //Cifrar claveSecreta con clave pública del profesor
+        byte[] claveCifrada = cifrarClave(claveSecreta);
+        System.out.println("Clave cifrada: " + claveCifrada);
+        System.out.println(bytesToHex(claveCifrada));
 
-        //Empaquetar examen y firma
+        //Hash de la claveSecreta cifrada
+        byte[] hashClaveSecretaExamenCifrado = hashClaveSecretaCifrada(claveCifrada, examenCifrado);
+        System.out.println("Hash: " + hashClaveSecretaExamenCifrado);
+        System.out.println(bytesToHex(hashClaveSecretaExamenCifrado));
+
+        //Cifrar hash clave publica alumno
+        byte[] hashCifrado = cifrarHashClavePrivadaAlumno(hashClaveSecretaExamenCifrado);
+        System.out.println("Hash cifrado: " + hashCifrado);
+        System.out.println(bytesToHex(hashCifrado));
+
+        //Empaquetar
         Paquete paquete = new Paquete();
-        paquete.anadirBloque("firma", firmaExamen); //examen cifrado clave privada del alumno
 
-        //Volcar paquete a fichero
-        paquete.escribirPaquete("examen.paquete");
+        paquete.anadirBloque("examenCifrado", examenCifrado);
+        paquete.anadirBloque("claveCifrada", claveCifrada);
+        paquete.anadirBloque("hashCifrado", hashCifrado);
 
-
- */
-        
+        paquete.escribirPaquete("paquete.bin");
     }
 
-    public static void cifrarExamen() throws Exception{                                                 
-        
-
-    }
+    //SecretKey para cifrar el examen
 
     public static SecretKey generaSecretKey() throws Exception{
         //Crear clave simétrica
@@ -84,24 +59,91 @@ public class Empaquetar{
 
     }
 
+    public static byte[] cifrarExamen(SecretKey claveSecreta, String inputFile) throws Exception{
+        //Cifrar examen con clave simétrica. Inicializar cifrador
+        Cipher cifrador = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        cifrador.init(Cipher.ENCRYPT_MODE, claveSecreta);
+        //Cargar archivo de entrada y preparar el de salida
+        FileInputStream in = new FileInputStream(inputFile);
+        CipherInputStream cifradoIn = new CipherInputStream(in, cifrador);
+        //Cifrar archivo
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int bytesLeidos;
+        while((bytesLeidos = cifradoIn.read(buffer)) != -1) {
+            //devolver byte[] cifrado
+            byteArrayOutputStream.write(buffer, 0, bytesLeidos);
+        }
+
+        in.close();
+        cifradoIn.close();
+        // Devolver byte[] cifrado
+        return byteArrayOutputStream.toByteArray();
+    }
+
     public static byte[] cifrarClave(SecretKey claveSecreta) throws Exception{
-        //Cifrar clave simétrica con la clave pública del profesor
+      
+         //Cifrar clave simétrica con la clave pública del profesor
         FileInputStream in = new FileInputStream("profesor.publica");
 
         byte[] encodedPKCS8 = new byte[in.available()];
         in.read(encodedPKCS8);
         in.close();
-        KeyFactory keyFactoryRSA = KeyFactory.getInstance("RSA", "BC");
-        PKCS8EncodedKeySpec publicKeySpec = new PKCS8EncodedKeySpec(encodedPKCS8);
-        PublicKey clavePublicaProfesor = keyFactoryRSA.generatePublic(publicKeySpec);
-
-        Cipher cifrar = Cipher.getInstance("RSA", "BC");
+        KeyFactory keyFactoryRSA = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec publiKeySpec = new X509EncodedKeySpec(encodedPKCS8);  // clave publica del profesor
+        //PKCS8EncodedKeySpec publicKeySpec = new PKCS8EncodedKeySpec(encodedPKCS8);  // clave publica del profesor
+        PublicKey clavePublicaProfesor = keyFactoryRSA.generatePublic(publiKeySpec);
+        Cipher cifrar = Cipher.getInstance("RSA/ECB/PKCS1Padding");//, "BC");
         cifrar.init(Cipher.ENCRYPT_MODE, clavePublicaProfesor);
         byte[] clave = claveSecreta.getEncoded();
-        byte[] claveCifrada = cifrar.update(clave);
         
-        return claveCifrada;
+        System.out.println("Clave getencoded: " + bytesToHex(clave)); //3d8a689d34aece1a
+        cifrar.update(clave);
+        byte[] fin = cifrar.doFinal();
+        return fin;//clave secreta cifrada con clave pública del profesor, para incluir en el paquete
+        
+    }
+    
+
+    public static byte[] hashClaveSecretaCifrada(byte[] claveCifrada, byte[] examenCifrado) throws Exception{ //parametro a pasar???
+        //Calcular hash del examen
+        MessageDigest hash = MessageDigest.getInstance("SHA-256");
+        hash.update(examenCifrado);
+        hash.update(claveCifrada);
+        byte[] hashClaveCifradaExamenCirfrado = hash.digest();
+        
+
+        return hashClaveCifradaExamenCirfrado;
     }
 
+    public static byte[] cifrarHashClavePrivadaAlumno(byte[] hashClaveCifradaExamenCirfrado) throws Exception{
+        FileInputStream in = new FileInputStream("alumno.privada");
+
+        byte[] encodedPKCS8 = new byte[in.available()];
+        in.read(encodedPKCS8);
+        in.close();
+        KeyFactory keyFactoryRSA = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPKCS8);  // clave publica del profesor
+        PrivateKey clavePrivadaAlumno = keyFactoryRSA.generatePrivate(privateKeySpec);
+        
+        Cipher cifrar = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cifrar.init(Cipher.ENCRYPT_MODE, clavePrivadaAlumno);
+        byte[] hash = hashClaveCifradaExamenCirfrado; //puede sobrar esta linea
+        cifrar.update(hash);
+        byte[] fin = cifrar.doFinal();
+        return fin;
+    }
+    
+
+
+
+    //curiosidad
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
     
 }
